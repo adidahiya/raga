09d284d61af0a64e7b5b1eaf9ee16d58f222f4a3
Adi Dahiya <adi.dahiya14@gmail.com>
2023-11-01 00:28:42 -0400

    update tsconfig, use ESM in main.ts

--- packages/music-library-app/src/main.ts
+++ packages/music-library-app/src/main.ts
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
+import { app, BrowserWindow } from "electron";
+import path from "node:path";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
