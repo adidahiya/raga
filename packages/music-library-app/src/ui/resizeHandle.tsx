0065a2a79f7cf1a1c4179a2079f67db114bb10d3
Adi Dahiya <adi.dahiya14@gmail.com>
2023-11-03 00:39:42 -0400

    adjust resize handle styling

--- packages/music-library-app/src/ui/resizeHandle.tsx
+++ packages/music-library-app/src/ui/resizeHandle.tsx
@@ -1,18 +1,12 @@
+import classNames from "classnames";
 import { PanelResizeHandle } from "react-resizable-panels";
 
 import styles from "./resizeHandle.module.scss";
 
 export default function ResizeHandle({ className = "", id }: { className?: string; id?: string }) {
     return (
-        <PanelResizeHandle className={[styles.ResizeHandleOuter, className].join(" ")} id={id}>
-            <div className={styles.ResizeHandleInner}>
-                <svg className={styles.Icon} viewBox="0 0 24 24">
-                    <path
-                        fill="currentColor"
-                        d="M8,18H11V15H2V13H22V15H13V18H16L12,22L8,18M12,2L8,6H11V9H2V11H22V9H13V6H16L12,2Z"
-                    />
-                </svg>
-            </div>
+        <PanelResizeHandle className={classNames(styles.outer, className)} id={id}>
+            <div className={styles.inner} />
         </PanelResizeHandle>
     );
 }
