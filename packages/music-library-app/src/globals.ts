e93c3c25ce0b90c4016044fc1a429bc9cb979804
Adi Dahiya <adi.dahiya14@gmail.com>
2023-12-17 15:56:54 -0500

    [app] chore: remove typescript-css-modules plugin, fix custom module type decl

--- packages/music-library-app/src/globals.ts
+++ packages/music-library-app/src/globals.ts
@@ -1,10 +1,5 @@
 import type { RoarrGlobalState } from "roarr";
 
-declare module "*.scss" {
-    const styles: Record<string, string>;
-    export default styles;
-}
-
 declare global {
     // eslint-disable-next-line no-var
     var ROARR: RoarrGlobalState;
