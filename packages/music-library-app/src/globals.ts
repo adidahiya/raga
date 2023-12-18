19d30ff3cf05a5a103738f62f60879dc41ee08a5
Adi Dahiya <adi.dahiya14@gmail.com>
2023-12-18 09:05:00 -0500

    [app] chore: code comment for TS limitation

--- packages/music-library-app/src/globals.ts
+++ packages/music-library-app/src/globals.ts
@@ -1,5 +1,7 @@
 import type { RoarrGlobalState } from "roarr";
 
+// N.B. this must live in a separate file from `declare module "*.scss"` because of a TypeScript limitation
+// see https://github.com/microsoft/TypeScript/issues/28097
 declare global {
     // eslint-disable-next-line no-var
     var ROARR: RoarrGlobalState;
