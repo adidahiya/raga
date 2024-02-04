8c95cb75bf2ebdcd9059b2847cd94ff42e64b2b9
Adi Dahiya <adi.dahiya14@gmail.com>
2024-02-03 22:39:33 -0500

    feat: use framer-motion to animate BPM overlay collapse transition

--- packages/raga-app/src/client/components/audioPlayer/trackBPMOverlay.tsx
+++ packages/raga-app/src/client/components/audioPlayer/trackBPMOverlay.tsx
@@ -1,6 +1,8 @@
 import type { TrackDefinition } from "@adahiya/raga-lib";
 import { Button, Classes, Slider } from "@blueprintjs/core";
+import { CaretLeft } from "@blueprintjs/icons";
 import classNames from "classnames";
+import { AnimatePresence, motion } from "framer-motion";
 import { debounce } from "radash";
 import { useCallback } from "react";
 import { useBoolean } from "usehooks-ts";
@@ -14,15 +16,6 @@ export function TrackBPMOverlay({ trackDef }: { trackDef: TrackDefinition }) {
   const setPlaybackRate = debounce({ delay: 5 }, appStore.use.setAudioPlaybackRate());
   const { value: isTempoSliderOpen, toggle: toggleTempoSlider } = useBoolean(false);
 
-  const tempoSliderValue = roundToNearestTenth(playbackRate * 100 - 100);
-  const handleTempoSliderChange = useCallback(
-    (value: number) => {
-      const roundedValue = roundToNearestTenth(value);
-      const playbackRate = (roundedValue + 100) / 100;
-      setPlaybackRate(playbackRate);
-    },
-    [setPlaybackRate],
-  );
   const handleDoubleClick = useCallback(() => {
     // reset playback rate to 1
     setPlaybackRate(1);
@@ -30,36 +23,44 @@ export function TrackBPMOverlay({ trackDef }: { trackDef: TrackDefinition }) {
 
   return (
     <div className={styles.bpmOverlay}>
-      <div className={styles.tempoSliderContainer} onDoubleClick={handleDoubleClick}>
-        {isTempoSliderOpen ? (
-          <>
-            <Slider
-              min={-10}
-              max={10}
-              stepSize={0.1}
-              labelRenderer={renderTempoSliderLabel}
-              labelValues={[-10, 0, 10]}
-              value={tempoSliderValue}
-              onChange={handleTempoSliderChange}
-            />
-            <Button
-              className={styles.tempoSliderToggleButton}
-              outlined={true}
-              small={true}
-              icon="caret-right"
-              onClick={toggleTempoSlider}
-            />
-          </>
-        ) : (
-          <Button
-            className={styles.tempoSliderToggleButton}
-            outlined={true}
-            small={true}
-            icon="caret-left"
-            onClick={toggleTempoSlider}
-          />
-        )}
-      </div>
+      <motion.div
+        className={styles.tempoSliderContainer}
+        onDoubleClick={handleDoubleClick}
+        animate={{ opacity: 1, width: isTempoSliderOpen ? "auto" : 0 }}
+        initial={{ opacity: 0 }}
+        transition={{ type: "tween", when: "afterChildren", duration: 0.2, ease: "easeInOut" }}
+        layout={true}
+      >
+        <AnimatePresence>
+          {isTempoSliderOpen && (
+            <motion.div
+              className={styles.tempoSlider}
+              initial={{ opacity: 0, scaleX: 0.5 }}
+              animate={{ opacity: 1, scaleX: 1 }}
+              exit={{ opacity: 0, scaleX: 0.5, transition: { delay: 0, duration: 0.1 } }}
+              transition={{
+                type: "tween",
+                delay: 0.05,
+                duration: 0.2,
+                ease: "easeInOut",
+              }}
+            >
+              <TrackTempoSlider />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+      <Button
+        className={styles.tempoSliderToggleButton}
+        outlined={true}
+        small={true}
+        icon={
+          <motion.span animate={{ rotate: isTempoSliderOpen ? 180 : 0 }}>
+            <CaretLeft />
+          </motion.span>
+        }
+        onClick={toggleTempoSlider}
+      />
       <span className={Classes.TEXT_MUTED}>BPM: </span>
       {trackDef.BPM !== undefined ? (
         <span
@@ -76,6 +77,33 @@ export function TrackBPMOverlay({ trackDef }: { trackDef: TrackDefinition }) {
   );
 }
 TrackBPMOverlay.displayName = "TrackBPMOverlay";
+
+function TrackTempoSlider() {
+  const playbackRate = appStore.use.audioPlaybackRate();
+  const setPlaybackRate = debounce({ delay: 5 }, appStore.use.setAudioPlaybackRate());
+
+  const tempoSliderValue = roundToNearestTenth(playbackRate * 100 - 100);
+  const handleTempoSliderChange = useCallback(
+    (value: number) => {
+      const roundedValue = roundToNearestTenth(value);
+      const playbackRate = (roundedValue + 100) / 100;
+      setPlaybackRate(playbackRate);
+    },
+    [setPlaybackRate],
+  );
+
+  return (
+    <Slider
+      min={-10}
+      max={10}
+      stepSize={0.1}
+      labelRenderer={renderTempoSliderLabel}
+      labelValues={[-10, 0, 10]}
+      value={tempoSliderValue}
+      onChange={handleTempoSliderChange}
+    />
+  );
+}
 
 const bpmFormatter = Intl.NumberFormat(undefined, {
   style: "decimal",
