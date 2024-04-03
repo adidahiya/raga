d0842d6bd09dfd0300f5b6ce173ecc616e8b8b85
Adi Dahiya <adi.dahiya14@gmail.com>
2024-04-03 10:36:52 -0400

    fix: stop using deprecated ansis function

--- packages/raga-lib/src/utils/log.ts
+++ packages/raga-lib/src/utils/log.ts
@@ -6,24 +6,24 @@ import { blue, cyan, gray, magenta, red, yellow } from "ansis";
  */
 export const log = {
   debug: (msg: string) => {
-    console.debug(`${magenta.inverse` debug `} ${cyan.faint`[lib]`} ${msg}`);
+    console.debug(`${magenta.inverse` debug `} ${cyan.dim`[lib]`} ${msg}`);
   },
   info: (msg: string) => {
-    console.debug(`${blue.inverse` info  `} ${cyan.faint`[lib]`} ${msg}`);
+    console.debug(`${blue.inverse` info  `} ${cyan.dim`[lib]`} ${msg}`);
   },
   warn: (msg: string) => {
-    console.debug(`${yellow.inverse` warn  `} ${cyan.faint`[lib]`} ${msg}`);
+    console.debug(`${yellow.inverse` warn  `} ${cyan.dim`[lib]`} ${msg}`);
   },
   error: (msg: string) => {
-    console.debug(`${red.inverse` error `} ${cyan.faint`[lib]`} ${msg}`);
+    console.debug(`${red.inverse` error `} ${cyan.dim`[lib]`} ${msg}`);
   },
   trace: (msg: string) => {
-    console.debug(`${gray.inverse` trace `} ${cyan.faint`[lib]`} ${msg}`);
+    console.debug(`${gray.inverse` trace `} ${cyan.dim`[lib]`} ${msg}`);
   },
   time: (label: string) => {
-    console.time(`${gray.inverse` time  `} ${cyan.faint`[lib]`} ${label}`);
+    console.time(`${gray.inverse` time  `} ${cyan.dim`[lib]`} ${label}`);
   },
   timeEnd: (label: string) => {
-    console.timeEnd(`${gray.inverse` time  `} ${cyan.faint`[lib]`} ${label}`);
+    console.timeEnd(`${gray.inverse` time  `} ${cyan.dim`[lib]`} ${label}`);
   },
 };
