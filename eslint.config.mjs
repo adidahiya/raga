c7e02ea06539df8a3736364313b0752a86654239
Adi Dahiya <adi.dahiya14@gmail.com>
2024-08-04 14:27:10 -0400

    chore: yarn upgrade & dedupe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -42,6 +42,7 @@ export default tseslint.config(
         },
       ],
       "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unnecessary-type-parameters": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
