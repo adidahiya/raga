e1dd49e253901aab73019456891b442b898a4989
Adi Dahiya <adi.dahiya14@gmail.com>
2025-06-18 09:27:41 -0400

    chore: upgrade dev deps

--- packages/raga-app/src/globals.ts
+++ packages/raga-app/src/globals.ts
@@ -3,7 +3,6 @@ import type { RoarrGlobalState } from "roarr";
 // N.B. this must live in a separate file from `declare module "*.scss"` because of a TypeScript limitation
 // see https://github.com/microsoft/TypeScript/issues/28097
 declare global {
-  // eslint-disable-next-line no-var
   var ROARR: RoarrGlobalState;
 
   type PartialRecord<K extends string | number, T> = Partial<Record<K, T>>;
